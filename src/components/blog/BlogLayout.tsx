4ba79aee9951a0e03c7e2d18eb2c895193b8a687
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2024-02-09 16:33:11 +0530

    add scroll to top button

--- src/components/blog/BlogLayout.tsx
+++ src/components/blog/BlogLayout.tsx
@@ -1,21 +1,55 @@
 import Prism from "prismjs"
 import "prismjs/themes/prism.css"
 import * as React from "react"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { Container, IconButton, Tooltip } from "@chakra-ui/react"
+import { ChevronUpIcon } from "@chakra-ui/icons"
 
 import "../../styles/blog.css"
 import Layout from "../Layout"
-import { Container } from "@chakra-ui/react"
 
 function BlogLayout({ children }) {
+  const [showScrollToTop, setShowScrollToTop] = useState(false)
+
   useEffect(() => {
     Prism.highlightAll()
   })
+
+  useEffect(() => {
+    const checkScrollTop = () => {
+      if (!showScrollToTop && window.pageYOffset > 400) {
+        setShowScrollToTop(true)
+      } else if (showScrollToTop && window.pageYOffset <= 400) {
+        setShowScrollToTop(false)
+      }
+    }
+    window.addEventListener("scroll", checkScrollTop)
+    return () => window.removeEventListener("scroll", checkScrollTop)
+  }, [showScrollToTop])
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+  }
 
   return (
     <Layout>
       <Container maxW="container.md" mt={["5", "10"]} mb={["5", "10"]}>
         {children}
+        {showScrollToTop && (
+          <Tooltip label="Scroll to top" aria-label="Scroll to top">
+            <IconButton
+              backgroundColor={"black"}
+              size={"lg"}
+              onClick={scrollToTop}
+              icon={<ChevronUpIcon color={"white"} fontSize={"4xl"} />}
+              position="fixed"
+              bottom="1rem"
+              right="1rem"
+              aria-label={"Scroll to top"}
+              _hover={{ backgroundColor: "gray" }}
+            />
+          </Tooltip>
+        )}
       </Container>
     </Layout>
   )
